23d2b2c7da17a7c778740a904cff752104887922
Alexander <allogydigital@gmail.com>
2025-02-10 12:12:19 +0200

    Add files via upload

---
+++ Computer.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.Win32;
+using System.Management;
+using System.Runtime.InteropServices;
+
+namespace neosharp;
+
+public static partial class Computer
+{
+    public const string LogoWin11 = @"
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+ 
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll";
+
+    public const string LogoWin10 = @"
+                            .oodMMMM
+                   .oodMMMMMMMMMMMMM
+       ..oodMMM  MMMMMMMMMMMMMMMMMMM
+ oodMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM
+ MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM
+ MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM
+ MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM
+ MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM
+ MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM
+ 
+ MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM
+ MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM
+ MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM
+ MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM
+ MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM
+ `^^^^^^MMMMMMM  MMMMMMMMMMMMMMMMMMM
+       ````^^^^  ^^MMMMMMMMMMMMMMMMM
+                      ````^^^^^^MMMM";
+
+    public static string GetMotherboard()
+    {
+        ManagementObject obj = new ManagementObjectSearcher("select Product from Win32_BaseBoard").Get().Cast<ManagementObject>().First();
+        return obj["Product"]?.ToString() ?? "Unknown";
+    }
+
+    public static string GetOperatingSystem()
+    {
+        RegistryKey? reg = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion");
+        string edition = reg?.GetValue("EditionID") as string ?? "Edition Unknown";
+        string version = reg?.GetValue("DisplayVersion") as string ?? "Unknown";
+
+        return $"Windows {Environment.OSVersion.Version.Major} {edition} [{version}, {(Environment.Is64BitOperatingSystem ? "x64" : "x86")}]";
+    }
+
+    public static string GetBuild() => Registry.GetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion", "CurrentBuild", "0")?.ToString() ?? "Unknown";
+
+    public static DateTime GetOSInstallDate() =>  DateTime.UnixEpoch + TimeSpan.FromSeconds(double.Parse(Registry.GetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion", "InstallDate", "0")?.ToString() ?? "0"));
+
+    public static TimeSpan GetUptime() => TimeSpan.FromMilliseconds(GetTickCount64());
+
+    public static string GetResolution()
+    {
+        ManagementObject obj = new ManagementObjectSearcher($"select CurrentHorizontalResolution, CurrentVerticalResolution from Win32_VideoController where Name = '{GetPrimaryGPU()}'").Get().Cast<ManagementObject>().First();
+        string horizontalResolution = obj["CurrentHorizontalResolution"]?.ToString() ?? "0";
+        string verticalResolution = obj["CurrentVerticalResolution"]?.ToString() ?? "0";
+        return horizontalResolution + "x" + verticalResolution;
+    }
+
+    public static string GetCPU()
+    {
+        ManagementObject obj = new ManagementObjectSearcher("select Name from Win32_Processor").Get().Cast<ManagementObject>().First();
+        return obj["Name"]?.ToString() ?? "Unknown";
+    }
+
+    public static string GetPrimaryGPU()
+    {
+        ManagementObject obj = new ManagementObjectSearcher("select Name from Win32_VideoController where MaxRefreshRate != NULL and MaxRefreshRate != \"\"").Get().Cast<ManagementObject>().First();
+        return obj["Name"]?.ToString() ?? "Unknown";
+    }
+
+    public static string GetMemory()
+    {
+        ManagementObject obj = new ManagementObjectSearcher("select TotalVisibleMemorySize, FreePhysicalMemory from Win32_OperatingSystem").Get().Cast<ManagementObject>().First();
+        string totalMemoryStr = obj["TotalVisibleMemorySize"].ToString() ?? "";
+        string freeMemoryStr = obj["FreePhysicalMemory"].ToString() ?? "";
+
+        if (string.IsNullOrEmpty(totalMemoryStr) || string.IsNullOrEmpty(freeMemoryStr))
+            return "Unknown";
+        
+        double total = double.Parse(totalMemoryStr);
+        double free = double.Parse(freeMemoryStr);
+        double used = total - free;
+        double usingPrec = Math.Floor(used / total * 100d);
+
+        return $"{used / 1074000:N2} GiB / {total / 1074000:N2} GiB ({usingPrec}%)";
+    }
+
+    public static string GetTheme()
+    {
+        RegistryKey? reg = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Themes");
+        string theme = reg?.GetValue("CurrentTheme") as string ?? "Unknown";
+        return theme.Split('\\').Last().Split('.').First();
+    }
+
+    public static string GetUsername() => Environment.UserName + "@" + Environment.MachineName;
+
+    
+    [LibraryImport("kernel32")]
+    private static partial ulong GetTickCount64();
+}
---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿using System.Diagnostics;
+
+namespace neosharp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Process p = Process.GetCurrentProcess();    
+        
+        if (Environment.GetEnvironmentVariable("WT_SESSION") != null || Environment.GetEnvironmentVariable("WT_PROFILE_ID") != null || p.ProcessName == "OpenConsole")
+            Console.Clear(); // Clear console if running in Windows Terminal to avoid buffer issues
+
+        Dictionary<string, string> data = new()
+        {
+            {Computer.GetUsername(), "Name"},
+            {new string('-', Computer.GetUsername().Length), "Separator"},
+            {"OS", Computer.GetOperatingSystem()},
+            {"Build", Computer.GetBuild()},
+            {"Installation date", Computer.GetOSInstallDate().ToString()},
+            {"Motherboard", Computer.GetMotherboard()},
+            {"Uptime", Computer.GetUptime().ToString(@"d\d\ hh\h\ mm\m\ ss\s")},
+            {"Resolution", Computer.GetResolution()},
+            {"Theme", Computer.GetTheme()},
+            {"CPU", Computer.GetCPU()},
+            {"Primary GPU", Computer.GetPrimaryGPU()},
+            {"Memory", Computer.GetMemory()}
+        };
+
+        Console.WriteLine();
+        (int, int) before = Console.GetCursorPosition();
+        string logoTransformed = LayoutAscii(Environment.OSVersion.Version.Major == 10 ? Computer.LogoWin10 : Computer.LogoWin11);
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine(logoTransformed);
+        (int, int) after = Console.GetCursorPosition();
+        PrintData(data, logoTransformed, before);
+        Console.SetCursorPosition(after.Item1, after.Item2);
+        Console.ResetColor();
+        
+        if (before.Item2 == 1)
+            Console.ReadKey();
+    }
+
+    static void PrintData(Dictionary<string, string> data, string layedOutascii, (int, int) pos)
+    {
+        char[] delims = ['\r', '\n'];
+        string[] strings = layedOutascii.Split(delims, StringSplitOptions.RemoveEmptyEntries);
+        int longestString = strings.Max(s => s.Length);
+
+        for (int i = 0; i < data.Count; i++)
+        {
+            KeyValuePair<string, string> pair = data.ElementAt(i);
+            Console.SetCursorPosition(longestString, pos.Item2 + i);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            if (pair.Value == "Name")
+            {
+                string[] split = pair.Key.Split('@');
+                Console.Write(split[0]);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write('@');
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(split[1]);
+                continue;
+            }
+            else if (pair.Value == "Separator")
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(pair.Key);
+                continue;
+            }
+
+            Console.Write(pair.Key);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write($": {pair.Value}");
+        }
+
+        PrintColorbar((longestString, pos.Item2 + data.Count));
+    }
+
+    static void PrintColorbar((int, int) pos)
+    {
+
+        Console.SetCursorPosition(pos.Item1, pos.Item2 + 2);
+
+        ConsoleColor[] colors = (ConsoleColor[])Enum.GetValues(typeof(ConsoleColor));
+
+        for (int i = 0; i < colors.Length; i++)
+        {
+            if (i == colors.Length / 2)
+                Console.SetCursorPosition(pos.Item1, pos.Item2 + 3);
+
+            ConsoleColor color = colors[i];
+            Console.BackgroundColor = color;
+            Console.Write(new string(' ', 3));
+        }
+    }
+
+    static string LayoutAscii(string text)
+    {
+        int spaces = 6;
+        char[] delims = ['\r', '\n'];
+        string[] strings = text.Split(delims, StringSplitOptions.RemoveEmptyEntries);
+        int longestString = strings.Max(s => s.Length);
+        string result = "";
+
+        foreach (string line in strings)
+        {
+            int spacesAdded = spaces + (longestString - line.Length);
+            result += ' ' + line + new string(' ', spacesAdded) + Environment.NewLine;
+        }
+
+        return result;
+    }
+}
